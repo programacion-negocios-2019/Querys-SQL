c987d6731745c4cb4efe8f5468f184c580d16aa1
Héctor Sabillón <h.hsabillon@gmail.com>
2019-06-19 12:32:08 -0600

    Agregar tabla Animal y tabla de relación entre zoológicos y animales.

--- Base de datos Zoológico.sql
+++ Base de datos Zoológico.sql
@@ -32,3 +32,35 @@ CREATE TABLE Zoo.Zoologico (
 	ciudad NVARCHAR(50) NOT NULL
 )
 GO
+
+-- Crear la tabla Animal
+CREATE TABLE Zoo.Animal (
+	id INT NOT NULL IDENTITY
+		CONSTRAINT PK_Animal_id PRIMARY KEY CLUSTERED,
+	nombre NVARCHAR(50) NOT NULL
+)
+GO
+
+-- Crear la tabla de relación entre los Zoológicos y los Animales
+CREATE TABLE Zoo.AnimalZoologico (
+	idZoologico INT NOT NULL,
+	idAnimal INT NOT NULL
+)
+GO
+
+-- Relaciones
+ALTER TABLE Zoo.AnimalZoologico
+	ADD CONSTRAINT
+		FK_Zoo_AnimalZoologico$Pertenece$Zoo_Zoologico_id
+		FOREIGN KEY (idZoologico) REFERENCES Zoo.Zoologico(id)
+		ON UPDATE CASCADE
+		ON DELETE NO ACTION
+GO
+
+ALTER TABLE Zoo.AnimalZoologico
+	ADD CONSTRAINT
+		FK_Zoo_AnimalZoologico$EsUn$Zoo_Animal_id
+		FOREIGN KEY (idAnimal) REFERENCES Zoo.Animal(id)
+		ON UPDATE CASCADE
+		ON DELETE NO ACTION
+GO
